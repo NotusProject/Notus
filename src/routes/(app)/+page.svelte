c76ba26d19724e4ee7b18d5d182e0dfb7b091d22
wasim <wasimysdev@gmail.com>
2024-05-18 16:09:26 +0200

    Add capabilities for desktop and android, update Tauri app settings, and enhance UI
    This commit includes the addition of capabilities for desktop and Android applications. It also integrates the Tauri plugin for OS and updates the config settings of the Tauri application. Additionally, the UI has been significantly improved with the introduction of a new Titlebar and theme switch. Tauri app name and version have been also updated.

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-ddddo
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<script lang="ts">
+    import ThemeSwitch from "./theme-switch.svelte"
+    import { WindowTitlebar} from "@tauri-controls/svelte";
+
+    const platforms = ["gnome"]
+</script>
+
+<svelte:head>
+    <title>Demo-Svelte</title>
+</svelte:head>
+
+
+
+            <WindowTitlebar
+                    controlsOrder="platform"
+                    class="h-10 rounded-t-lg  bg-white shadow dark:bg-slate-800"
+                    data-tauri-drag-region
+            >
+                <div class="ml-3 flex items-center" data-tauri-drag-region>
+                    <div
+                            class="flex w-full items-center justify-center"
+                            data-tauri-drag-region
+                    >
+                        Title
+                    </div>
+                    <div
+                            class="flex flex-row space-x-2 whitespace-nowrap rounded-md px-2 py-1 shadow"
+                    >
+
+                    </div>
+                </div>
+
+
+            </WindowTitlebar>
+
+
+
+
+<style lang="postcss">
+    :global(html.dark) {
+        background-color: theme(colors.sky.950);
+    }
+    :global(html) {
+        background-color: theme(colors.slate.50);
+    }
+</style>
