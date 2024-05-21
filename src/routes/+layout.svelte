dd01c07439eee78fb417f995b3142d9250797a05
wasim <wasimysdev@gmail.com>
2024-05-21 08:11:48 +0200

    fixed the bug with titlebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -40,9 +40,9 @@
 <!--                </div>-->
             </div>
             {#if user}
-                <div  class="absolute  left-1/2 transform top-1 -translate-x-1/2 lex w-96 mx-auto font-medium text-xs text-slate-400 items-center justify-center">
+                <div  class="absolute z-50  left-1/2 transform top-1 -translate-x-1/2 lex w-96 mx-auto font-medium text-xs text-slate-400 items-center justify-center">
                     <input type="text" name="search" id="search"
-                           class="block w-full rounded-md  border-0 py-1 bg-slate-600/25 text-white  ring-inset text-xs ring-gray-500 placeholder:text-gray-400  focus:ring-1 focus:ring-inset focus:ring-gray-500 focus:bg-gray-300/5 hover:bg-gray-300/10 sm:text-sm sm:leading-6"
+                           class="block w-full rounded-md  border-0 py-1 bg-slate-400/20 text-white  ring-inset text-xs ring-gray-500 placeholder:text-gray-400  focus:ring-1 focus:ring-inset focus:ring-gray-500 focus:bg-gray-300/15 hover:bg-gray-300/25 sm:text-sm sm:leading-6"
                            placeholder="search...">
                     <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
                         <kbd class="inline-flex items-center rounded border border-slate-400 px-1 font-sans text-xs text-gray-400">Ctrl+K</kbd>
